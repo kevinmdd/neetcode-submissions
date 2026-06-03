8f7abaad5dfd2b64a1474d184b561ffc4e54ca30
kevinmdd <151257451+kevinmdd@users.noreply.github.com>
2026-06-02 21:18:15 -0700

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        r = {}
+        for num in nums:
+            if num not in r:
+                r[num] = nums.count(num)
+                if len(r) > k:
+                    lowest_num = min(r, key=r.get)
+                    del r[lowest_num]
+        return list(r.keys())
+            
+        
