a3bca74ca8e4d99244d2f3f4f5319baf7070b773
kevinmdd <151257451+kevinmdd@users.noreply.github.com>
2026-08-22 17:23:18 -0700

    Add: valid-sudoku - submission-13

---
+++ Data Structures & Algorithms/valid-sudoku/submission-13.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        check = [set() for _ in range(9)]
+        column_check = [set() for _ in range(9)]
+        key_increment = 0 
+        new_box = 0
+        for i in range(len(board)):
+            if (i % 3) == 0 and i > 0:
+                new_box = i
+            key_increment = new_box
+            row_check = set()
+            for j in range(len(board)):
+                if (j % 3) == 0 and j > 0:
+                    key_increment += 1
+                if board[i][j] == '.':
+                    continue
+                if board[i][j] in check[key_increment]:
+                    print(f"invalid 3x3 value: {board[i][j]}")
+                    return False
+                if board[i][j] in row_check:
+                    print(f"invalid row value: {board[i][j]}")
+                    return False
+                if board[i][j] in column_check[j]:
+                    print(f"invalid column value: {board[i][j]}")
+                    return False
+                check[key_increment].add(board[i][j])
+                row_check.add(board[i][j])
+                column_check[j].add(board[i][j])
+        return True
+                
+
