d971b87ddffd0395cd0dd6c6e0010bf5b937bd51
kevinmdd <151257451+kevinmdd@users.noreply.github.com>
2026-08-25 22:28:08 -0700

    Add: minimum-stack - submission-18

---
+++ Data Structures & Algorithms/minimum-stack/submission-18.py
@@ -0,0 +1,27 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.curr_min = float('inf')
+        self.min_list = []
+
+    def push(self, val: int) -> None:
+        if not self.min_list:
+            self.min_list.append(val)
+            self.curr_min = val
+        elif val <= self.curr_min:
+            self.curr_min = val
+            self.min_list.append(self.curr_min)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack[-1] == self.min_list[-1]:
+            self.min_list.pop()
+        self.stack.pop()
+
+    def top(self) -> int:   
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.min_list:         
+            return self.min_list[-1]
+        
